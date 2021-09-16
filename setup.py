577c86514a6ac33c8e7401e92af09c769eb8ddbc
Thomas Mandzie <tmanfree@hotmail.com>
2021-09-16 11:19:57 -0600

    Folder structure updating

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ DESCRIPTION = "Edge Web Interface"
 URL = "https://github.com/tmanfree/edgeweb"
 EMAIL = "tmanfree@hotmail.com"
 AUTHOR = "Thomas Mandzie"
-VERSION = "0.0.3"
+VERSION = "0.0.4"
 
 
 with open("README.md", "r") as fh:
